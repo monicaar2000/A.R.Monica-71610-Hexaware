b500c9506d7a98e2d61c57c0039d7ca1c8c4d017
monicaar2000 <72865333+monicaar2000@users.noreply.github.com>
2022-05-06 10:11:15 +0530

    Add files via upload

---
+++ Demo2.py
@@ -0,0 +1,28 @@
+class Emp:
+
+   '''def _init__(self,uid,name,age,salary,desig) :
+        self.uid=uid;
+        self.name=name;
+        self.age=age;
+        self.salary=salary;
+        self.desig=design;
+
+    def __init_-(self):
+        self.uid=input("Enter ID :");
+        self.name=input("Enter NAme  :");
+        self.age=input("Enter Age:");
+        self.salary=input("Enter SALARY :");
+        self.desig=input("Enter Desig:");
+
+
+    def dispaly(self):
+       print("Name :", self.name);
+       print("ID : ",self.uid);
+       print("Age :", self.age);
+       print("Salary: ",self.salary);
+       print("Design :",self.design);
+
+#e=Emp(100,"Sunil",35,40000,"clerk");
+e =Emp();
+e.display();
+             
